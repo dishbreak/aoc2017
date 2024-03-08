ba1d919b43dc2fb99081fcef198ce2be77978592
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2024-03-08 11:13:01 -0800

    day 10 part 1 complete

--- cmd/day10/main.go
+++ cmd/day10/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("inputs/day10.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	fmt.Printf("Part 1: %d\n", part1(f))
+	f.Seek(0, 0)
+}
+
 type knotHasher struct {
 	data []int
 	pos  int
@@ -39,4 +59,35 @@ func (k *knotHasher) hash(length int) {
 
 	k.pos = (k.pos + length + k.skip) % len(k.data)
 	k.skip++
+}
+
+func extract(r io.Reader) []int {
+	s := bufio.NewScanner(r)
+	s.Scan()
+	parts := strings.Split(s.Text(), ",")
+
+	result := make([]int, len(parts))
+
+	for i := range parts {
+		result[i], _ = strconv.Atoi(parts[i])
+	}
+
+	return result
+}
+
+func part1(r io.Reader) int {
+	lengths := extract(r)
+
+	data := make([]int, 256)
+	for i := range data {
+		data[i] = i
+	}
+
+	h := newKnotHasher(data)
+
+	for _, length := range lengths {
+		h.hash(length)
+	}
+
+	return h.data[0] * h.data[1]
 }
