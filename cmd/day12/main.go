95c1a26c4d21463374e14c3045b748a4019d819f
Vishal Kotcherlakota <dishbreak@gmail.com>
2024-03-26 09:01:47 -0700

    day 12 part 2 complete

--- cmd/day12/main.go
+++ cmd/day12/main.go
@@ -14,9 +14,24 @@ func main() {
 	defer f.Close()
 
 	fmt.Printf("Part 1: %d\n", part1(f))
+	f.Seek(0, 0)
+	fmt.Printf("Part 2: %d\n", part2(f))
 }
 
 func part1(r io.Reader) int {
 	g := parse(r)
 	return g.trace(0)
+}
+
+func part2(r io.Reader) int {
+	g := parse(r)
+	acc := 0
+
+	for id := range g {
+		if g.trace(id) != -1 {
+			acc++
+		}
+	}
+
+	return acc
 }
