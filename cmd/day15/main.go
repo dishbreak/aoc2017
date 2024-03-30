ec89e605007e84908c04a37b5ca4e9e4c4274fd1
Vishal Kotcherlakota <dishbreak@gmail.com>
2024-03-30 00:46:03 -0700

    finish refactor

--- cmd/day15/main.go
+++ cmd/day15/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"os"
-	"regexp"
-	"strconv"
+
+	"github.com/dishbreak/aoc2017/cmd/day15/part1"
+	"github.com/dishbreak/aoc2017/cmd/day15/part2"
 )
 
 func main() {
@@ -15,52 +14,8 @@ func main() {
 		panic(err)
 	}
 	defer f.Close()
-	fmt.Printf("Part 1: %d\n", part1(f))
-}
 
-var factors = map[string]int{
-	"A": 16807,
-	"B": 48271,
-}
-
-const moduloValue = 2147483647
-
-type generator struct {
-	prev, factor int
-}
-
-var regexGeneratorLine = regexp.MustCompile(`^Generator (A|B) starts with (\d+)$`)
-
-func parseLine(s string) *generator {
-	matches := regexGeneratorLine.FindStringSubmatch(s)
-	startVal, _ := strconv.Atoi(matches[2])
-	factor := factors[matches[1]]
-
-	return &generator{startVal, factor}
-}
-
-func (g *generator) generate() int {
-	g.prev = (g.prev * g.factor) % moduloValue
-	return g.prev
-}
-
-const mask = (1 << 16) - 1
-
-func part1(r io.Reader) int {
-	acc := 0
-
-	s := bufio.NewScanner(r)
-	s.Scan()
-	genA := parseLine(s.Text())
-
-	s.Scan()
-	genB := parseLine(s.Text())
-
-	for i := 0; i < 40000000; i++ {
-		if a, b := genA.generate(), genB.generate(); (a & mask) == (b & mask) {
-			acc++
-		}
-	}
-
-	return acc
+	fmt.Printf("Part 1: %d\n", part1.Solve(f))
+	f.Seek(0, 0)
+	fmt.Printf("Part 2: %d\n", part2.Solve(f))
 }
--- cmd/day15/part2/part2_test.go
+++ cmd/day15/part2/part2_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPart1(t *testing.T) {
+func TestPart2(t *testing.T) {
 	input := `Generator A starts with 65
 Generator B starts with 8921`
 	r := strings.NewReader(input)
